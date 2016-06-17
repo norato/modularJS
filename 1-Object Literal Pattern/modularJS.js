5c3fc5f16cde49cc1b1cae829147b6f7cd3918dd
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-06-17 01:05:34 -0300

    Add first and second episode

--- 1-Object Literal Pattern/modularJS.js
+++ 1-Object Literal Pattern/modularJS.js
@@ -1,19 +1,61 @@
-var people = [];
-var template = $('#people-template').html();
+(function() {
+  var people = {
+    people: [],
+    init : function() {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function() {
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template= this.$el.find('#people-template').html();
+    },
+    bindEvents: function() {
+      this.$button.on('click', this.addPerson.bind(this));
+      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
+    },
+    render: function() {
+      var data = {
+        people: this.people,
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    addPerson: function() {
+      this.people.push(this.$input.val());
+      this.render();
+      this.$input.val('');
+    },
+    deletePerson: function(event) {
+      var $remove = $(event.target).closest('li');
+      var i = this.$ul.find('li').index($remove);
 
-$('#peopleModule').find('button').on('click', function () {
-  people.push($('#peopleModule').find('input').val());
-  $('#peopleModule').find('input').val('');
-  //data for mustache template
-  var data = {
-    people: people,
+      this.people.splice(i, 1);
+      this.render();
+    }
   };
-  $('#peopleModule').find('ul').html(Mustache.render(template, data));
-})
-$('#peopleModule').find('ul').delegate('i.del', 'click', function (e) {
-  var $remove = $(e.target).closest('li');
-  var i = $('#peopleModule').find('ul').find('li').index($remove);
 
-  $remove.remove();
-  people.splice(i, 1);
-});
+  people.init();
+})();
+
+// var people = [];
+// var template = $('#people-template').html();
+
+// $('#peopleModule').find('button').on('click', function () {
+//   people.push($('#peopleModule').find('input').val());
+//   $('#peopleModule').find('input').val('');
+//   //data for mustache template
+//   var data = {
+//     people: people,
+//   };
+//   $('#peopleModule').find('ul').html(Mustache.render(template, data));
+// })
+// $('#peopleModule').find('ul').delegate('i.del', 'click', function (e) {
+//   var $remove = $(e.target).closest('li');
+//   var i = $('#peopleModule').find('ul').find('li').index($remove);
+
+//   $remove.remove();
+//   people.splice(i, 1);
+// });
